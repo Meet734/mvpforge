f5e1a7ba7f8d43ed306cb5964ac5b683a8dd1689
meet vaghasiya <95461453+Meet734@users.noreply.github.com>
2026-04-26 21:27:44 +0530

    Update settings.py

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -25,7 +25,7 @@ class Settings(BaseSettings):
         "http://localhost:8000",
         "https://mvpforge-backend.onrender.com",
         "https://mvpforge2-1rx0oax2y-meet734s-projects.vercel.app",
-        "mvpforge2-9vw5x4kw6-meet734s-projects.vercel.app"
+        "https://mvpforge2-9vw5x4kw6-meet734s-projects.vercel.app"
     ]
 
     # Database
